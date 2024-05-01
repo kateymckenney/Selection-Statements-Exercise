444584128cfe8718a733af8525c8e8c5a7389192
Katey McKenney <mckenneykatey@gmail.com>
2024-05-01 11:13:37 -0500

    completed exercise 2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+
+
+            //----------------Exercise 1-------------------
+
+
             Console.WriteLine("Welcome to the number guessing game!");
             Console.WriteLine("Let's see if you can guess the correct number");
             Console.WriteLine("Pick a number between 1 and 15");
@@ -27,6 +32,50 @@
             {
                 Console.WriteLine("Correct!  You guessed it!");
             }
+
+
+
+            //---------------Exercise 2------------------
+
+            Console.WriteLine("What is your favorite school subject?");
+            string favSub = Console.ReadLine();
+
+            switch (favSub.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is cool because there is always something new to learn");
+            
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is great because it encourages us to ask questions, test, and try new things");
+
+                    break;
+
+                case "geography":
+                    Console.WriteLine("Geography helps us appreciate different areas and cultures around the world.");
+
+                    break;
+
+                case "art":
+                    Console.WriteLine("Art is a wonderful way to encourage self expression and creativity!");
+
+                    break;
+
+                case "languages":
+                    Console.WriteLine("Learninig new languages is one of the best ways to connect with people and cultures different from your own!");
+
+                    break;
+
+                default:
+                    Console.WriteLine("Wow, that is one of my favorites too!");
+
+                    break;
+
+            }
+
+
         }
+
     }
 }
